c478304125290b967f4672c52f83fe97b718f3e8
runapal5 <runapal5@gmail.com>
2017-09-29 23:29:07 +0530

    Update RunMeTask.java

--- src/main/java/com/niit/scheduler/RunMeTask.java
+++ src/main/java/com/niit/scheduler/RunMeTask.java
@@ -1,32 +1,34 @@
 package com.niit.scheduler;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RunMeTask {
 	public void printMe() {
-		System.out.println("Spring 3 + Quartz 1.8.6 ~");
+		System.out.println("Execute Query for Prediction");
 		executeScript();
 	}
 	
 	
 	public void executeScript() {
-		 try {
-		             String target = new String("/root/testResultPrediction.sh");
 		
-		             Runtime rt = Runtime.getRuntime();
-		             Process proc = rt.exec(target);
-		             proc.waitFor();
-		             StringBuffer output = new StringBuffer();
-		             BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-		             String line = "";                       
-		             while ((line = reader.readLine())!= null) {
-		                     output.append(line + "\n");
-		             }
-		             System.out.println("### " + output);
-		     } catch (Throwable t) {
-		             t.printStackTrace();
-		     }
+		 try {
+	            Process proc = Runtime.getRuntime().exec("/root/testResultPrediction.sh"); //Whatever you want to execute
+	            BufferedReader read = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+	                   
+	            try {
+	                proc.waitFor();
+	            } catch (InterruptedException e) {
+	                System.out.println(e.getMessage());
+	            }
+	            while (read.ready()) {
+	                System.out.println(read.readLine());
+	            }
+	        } catch (IOException e) {
+	            System.out.println(e.getMessage());
+	        }
+		 
 	}
 	
 }
